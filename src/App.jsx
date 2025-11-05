ff7445db699d16cc7645d654539c3304bcb58f39
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-05 12:24:30 +0900

    ⚙️ Chore: vite 번들러 세팅

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <title>타이틀 적용해보기~!</title>
+      <meta name="description" content="웹 페이지 설명 적용해보기~" />
+      <h1>테스트~</h1>
+    </>
+  );
+}
+
+export default App;
