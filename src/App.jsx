dbc763c5cbf0a3b5659191b0e29abeaadefe2f1c
apple <apple@apples-MacBook-Air.local>
2025-11-10 01:27:38 +0900

    추가 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import BioForm from './components/Bio';
-import EmailForm from './components/Email';
-import PasswordForm from './components/Password';
-
 function App() {
-  return (
-    <>
-      <EmailForm />
-      <PasswordForm />
-      <BioForm />
-      <h1 className="text-red-500 underline">Hello world!</h1>
-      <p className="text-red-500">빨강 글씨</p>
-      <p className="text-error">커스텀 빨강</p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
